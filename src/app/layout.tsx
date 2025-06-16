5bd0ab559525b26a77f10381c0d07a31813fc02d
Vishnu <iam.vishnu.bhaskar@gmail.com>
2025-06-16 19:55:34 +0530

    add loading progress in default layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,18 +1,12 @@
 import "@/css/satoshi.css";
 import "@/css/style.css";
 
-import { Sidebar } from "@/components/Layouts/sidebar";
-
 import "flatpickr/dist/flatpickr.min.css";
 import "jsvectormap/dist/jsvectormap.css";
 
-import { Header } from "@/components/Layouts/header";
 import type { Metadata } from "next";
 import NextTopLoader from "nextjs-toploader";
 import type { PropsWithChildren } from "react";
-import { Providers } from "./providers";
-import { headers } from "next/headers";
-import DefaultLayout from "@/layouts/DefaultLayout";
 
 export const metadata: Metadata = {
   title: {
@@ -23,16 +17,13 @@ export const metadata: Metadata = {
     "Next.js admin dashboard toolkit with 200+ templates, UI components, and integrations for fast dashboard development.",
 };
 
-const plainTemplateUrls = [
-  "/auth/sign-in",
-]
-
 export default async function RootLayout({ children }: PropsWithChildren) {
 
   // Return Dashboard Layout
   return (
     <html lang="en" suppressHydrationWarning>
       <body>
+        <NextTopLoader color="#5750F1" showSpinner={false} />
         <main className="isolate mx-auto w-full max-w-screen-2xl overflow-hidden p-4 md:p-6 2xl:p-10">
           {children}
         </main>
